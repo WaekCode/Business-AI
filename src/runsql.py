8fea0fb0c4e3ce70c6a048d4049066509e8fdf10
MjhX <mjhandrock@gmail.com>
2025-07-26 17:40:21 -0500

    Changed the file name from sql.py to runsql.py

--- src/runsql.py
+++ src/runsql.py
@@ -1,6 +1,12 @@
 import sqlite3
 from sqlite3 import Error
 
+def read_db(path):
+    f = open(path,"r")
+    out = f.readlines()
+    f.close()
+    return out
+
 def main():
     print("Hello World!")
 
