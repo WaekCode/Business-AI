d8d17f21ec4b9e8357df1bb5f0e5b98045671593
MjhX <mjhandrock@gmail.com>
2025-07-26 17:40:40 -0500

    Small updates to main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import sys
+import src.runsql
+import sqlite3
+
 def main():
-    print("Hello World!")
+    args = sys.argv[1:]
+    if len(args) > 0:
+        db = src.runsql.read_db(args[0])
+        print(db)
 
 if __name__ == "__main__":
     main()
