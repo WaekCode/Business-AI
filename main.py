85f18351d03d03cf147ffe721254e2ae93b2ec81
MjhX <mjhandrock@gmail.com>
2025-07-27 12:31:12 -0500

    Refactored runsql code

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
-import src.runsql
+from src import runsql
 import sqlite3
 
 def main():
     args = sys.argv[1:]
     if len(args) >= 1:
-        db = src.runsql.read_db(f"sql/{args[0]}.sql", f"db/{args[0]}.db")
+        db = runsql.open_conn(f"db/{args[0]}.db")
+        runsql.run_sql(db, f"sql/{args[0]}.sql")
         print(db)
+        runsql.close_conn(db)
 
 if __name__ == "__main__":
     main()
--- src/runsql.py
+++ src/runsql.py
@@ -2,21 +2,26 @@ import os
 import sqlite3
 from sqlite3 import Error
 
-def read_db(path,dbname):
-    f = open(path,"r")
-    out = f.read()
-    f.close()
+def open_conn(db_path):
     try:
-        os.makedirs("db", exist_ok=True)
-        conn = sqlite3.connect(dbname)
+        os.makedirs(os.path.dirname(db_path), exist_ok=True)
+        conn = sqlite3.connect(db_path)
+        return conn
+    except Error as e:
+        raise(e)
+
+def run_sql(conn, sql_path):
+    try:
+        f = open(sql_path,"r")
+        out = f.read()
+        f.close()
         cursor = conn.cursor()
         cursor.executescript(out)
-        conn.close()
-        return conn
-    except OSError as e:
+    except Error as e:
         raise(e)
-    except sqlite3.OperationalError as e:
-        raise(e)
+
+def close_conn(conn):
+    conn.close()
 
 def main():
     print("Call read.db from main")
